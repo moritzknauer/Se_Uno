236b07a3eb80cd07bcc4255a8ffcd030743267a8
moritzknauer <poelker.noering@gmx.de>
2020-05-28 18:34:14 +0200

    CardOptions aktualisiert

--- src/main/scala/CardOptions.scala
+++ src/main/scala/CardOptions.scala
@@ -3,22 +3,12 @@ package scala
 object CardOptions {
   object Color extends Enumeration {
     type Color = Value
-    val Red, Yellow, Green, Blue = Value
+    val Red, Yellow, Green, Blue, Black = Value
   }
 
   object Value extends Enumeration {
     type Values = Value
     val Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, DirectionChange,
-    Suspend, PlusTwo = Value
-  }
-
-  object Special extends Enumeration {
-    type Special = Value
-    val PlusFour, ColorChange , Uno = Value
-  }
-
-  object Black extends Enumeration {
-    type Black = Value
-    val Black = Value
+    Suspend, PlusTwo, PlusFour, ColorChange , Uno = Value
   }
 }
