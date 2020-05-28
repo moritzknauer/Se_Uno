829e1eca78edd8de65d76ef0314af6e1ef907e34
moritzknauer <poelker.noering@gmx.de>
2020-05-28 18:38:11 +0200

    Uno aktualisiert

--- src/main/scala/Uno.scala
+++ src/main/scala/Uno.scala
@@ -12,20 +12,10 @@ object Uno {
     val student = Player("Your Name")
     println("Hello, " + student.name)
 
-    val card1 = Card(Color.Blue, Value.Seven)
-
-    val card2 = Card(Color.Red, Value.Nine)
-
-    val card3 = Card(Color.Yellow, Value.DirectionChange)
-
-    val card4 = Card(Color.Red, Value.Suspend)
-
-    val card5 = Card(Color.Green, Value.PlusTwo)
-
     val cardsCovered: List[Card] = List()
     val cardsRevealed: List[Card] = List()
     val enemyCarsd: List[Card] = List()
-    val handCards: List[Card] = List(card4, card5)
+    val handCards: List[Card] = List()
 
     var cards = new ListBuffer[Card]()
 
@@ -39,14 +29,6 @@ object Uno {
         }
       }
     }
-
-
-    //val cardsCovered: List[Card] = List(card1, card2)
-    //val cardsRevealed: List[Card] = List(card3)
-    //val cardsRevealed: List[Card] = List(card3, card2)
-    //val cardsCovered: List[Card] = List(card1)
-
-    print(handCards)
 
     printf("\n")
     printf("\n")
