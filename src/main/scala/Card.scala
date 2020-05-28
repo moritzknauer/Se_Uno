71061126e8aad74adf23bdd3cbd17c25920e5c9c
moritzknauer <poelker.noering@gmx.de>
2020-05-28 18:32:35 +0200

    CardOptions aktualisiert

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -2,8 +2,6 @@ package scala
 
 import CardOptions.Color.Color
 import CardOptions.Value.Values
-import CardOptions.Special.Special
-import CardOptions.Black.Black
 
 
 case class Card(color:Color, value:Values){
