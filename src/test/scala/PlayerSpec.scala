a53867ac8e0396c07bf91bd2d93bdab4c4e4bbba
moritzknauer <poelker.noering@gmx.de>
2020-05-28 19:02:46 +0200

    Testklasse für Klasse Player

--- src/test/scala/PlayerSpec.scala
+++ src/test/scala/PlayerSpec.scala
@@ -1,15 +1,15 @@
-package scala
-
 import org.scalatest._
 
 case class PlayerSpec() extends WordSpec with Matchers {
-  "A Player" when { "new" should {
-    val player = Player("Your Name")
-    "have a name"  in {
-      player.name should be("Your Name")
+  "A Player" when {
+    "new" should {
+      val player = Player("Your Name")
+      "have a name"  in {
+        player.name should be("Your Name")
+      }
+      "have a nice String representation" in {
+        player.toString should be("Your Name")
+      }
     }
-    "have a nice String representation" in {
-      player.toString should be("Your Name")
-    }
-  }}
+  }
 }
