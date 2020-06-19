7a4bd7a7a47e588742ab921fcb51e6b818dee6cc
moritzknauer <poelker.noering@gmx.de>
2020-06-19 08:48:00 +0200

    Updatet for task 8

--- src/test/scala/de/htwg/se/uno/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/uno/model/PlayerSpec.scala
@@ -85,6 +85,18 @@ class PlayerSpec extends WordSpec {
       "be able to return a Card of the List of Hand Cards that equals the String" in{
         newGame.player.getCard(newGame.player.handCards(0).toString) should be (newGame.player.handCards(0))
       }
+      "be able to undo another step" in {
+        newGame.player.undo(newGame) should be(newGame.player)
+      }
+      "be able to undo a third step" in {
+        newGame.player.undo(newGame) should be(newGame.player)
+      }
+      "be able to do a push move" in {
+        newGame.player.pushMove(newGame.player.handCards.head.toString, newGame) should be(newGame.player)
+      }
+      "be able to do a pull Move" in {
+        newGame.player.pullMove(newGame) should be(newGame.player)
+      }
     }
   }
 }
