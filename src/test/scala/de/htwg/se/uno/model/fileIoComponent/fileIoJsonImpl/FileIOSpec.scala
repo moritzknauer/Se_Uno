bf4299fdcce846e0b4609a153012d4f714961273
moritzknauer <poelker.noering@gmx.de>
2020-07-08 19:38:35 +0200

    Updatet to test if SwingGuiSpec destroys tests

---
+++ src/test/scala/de/htwg/se/uno/model/fileIoComponent/fileIoJsonImpl/FileIOSpec.scala
@@ -0,0 +1,38 @@
+package de.htwg.se.uno.model.fileIoComponent.fileIoJsonImpl
+
+import de.htwg.se.uno.model.gameComponent.gameBaseImpl.Game
+import org.scalatest.Matchers._
+import org.scalatest._
+
+class FileIOSpec extends WordSpec {
+  "A FileIO" when {
+    "new" should {
+      var game = new Game(4)
+      val fileIo = new FileIO
+      "Should be able to save the game" in {
+        fileIo.save(game)
+      }
+      "Should be able to load the game" in {
+        fileIo.load
+      }
+      "Should be able to save the game again" in {
+        game = new Game(2)
+        game.createGame()
+        game.setDirection()
+        game.setActivePlayer()
+        fileIo.save(game)
+      }
+      "Should be able to load the game again" in {
+        fileIo.load
+      }
+      "Should be able to save the game a third time" in {
+        game = new Game(3)
+        game.createGame()
+        fileIo.save(game)
+      }
+      "Should be able to load the game a third time" in {
+        fileIo.load
+      }
+    }
+  }
+}
