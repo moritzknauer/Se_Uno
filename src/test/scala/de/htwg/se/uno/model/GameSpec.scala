86dd773ec8c47f4cfde96a1eee9df700840d5350
moritzknauer <poelker.noering@gmx.de>
2020-07-10 16:45:24 +0200

    Checked if Tests are needed

--- src/test/scala/de/htwg/se/uno/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/uno/model/GameSpec.scala
@@ -11,12 +11,10 @@ class GameSpec extends WordSpec {
   "A Game" when {
     "new" should {
       var newGame = Game(2)
-      /*
       "Should be able to create a Game" in {
         newGame = newGame.createGame()
         newGame.getLength(4) should be(7)
       }
-      */
       "Should be able to create a Testgame" in {
         newGame = newGame.createTestGame()
         newGame.getLength(4) should be(9)
